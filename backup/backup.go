a50f9e54e946136a689c8bd19d38c4dd142d1c6b
nderitualphie <aliphonzanderitu@gmail.com>
2023-06-23 09:57:21 +0300

    name

--- backup/backup.go
+++ backup/backup.go
@@ -19,13 +19,13 @@ func Backup() {
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/", username, password, hostname, port))
 	if err != nil {
 		fmt.Println("Error opening database connection: ", err)
-		return
+
 	}
 	defer db.Close()
 	rows, err := db.Query("SHOW DATABASES")
 	if err != nil {
 		fmt.Println("Error opening database: ", err)
-		return
+
 	}
 	defer rows.Close()
 
@@ -37,7 +37,7 @@ func Backup() {
 		var dbName string
 		if err := rows.Scan(&dbName); err != nil {
 			fmt.Println("Error scanning database name: ", err)
-			return
+
 		}
 
 		// Skip system databases
@@ -49,24 +49,25 @@ func Backup() {
 		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, hostname, port, dbName))
 		if err != nil {
 			fmt.Println("Error opening database connection: ", err)
-			return
+
 		}
+		dt := time.DateOnly
 		dumpDir := os.Getenv("BACKUP_DIR") // you should create this directory
-		dumpFilenameFormat := fmt.Sprintf("%s-%s.sql", dbName, time.Now().Format("20060102"))
+		dumpFilenameFormat := fmt.Sprintf("%s-%s", dbName, dt)
 		localPath = dumpDir + "/" + dumpFilenameFormat
 
 		// Register database with mysqldump
 		dumper, err := mysqldump.Register(db, dumpDir, dumpFilenameFormat)
 		if err != nil {
 			fmt.Println("Error registering database:", err)
-			return
+
 		}
 
 		// Dump database to file
 		resultFilename, err := dumper.Dump()
 		if err != nil {
 			fmt.Println("Error dumping:", err)
-			return
+
 		}
 		fmt.Printf("File is saved to %s\n", resultFilename)
 
@@ -75,11 +76,11 @@ func Backup() {
 
 		// Upload the file to S3 bucket
 		bucketName := os.Getenv("BUCKET_NAME")
-		remotePath := dumpFilenameFormat
+		remotePath := resultFilename
 		err = UploadObject(localPath, bucketName, remotePath)
 		if err != nil {
 			log.Printf("Error uploading to bucket: %v", err)
-			return
+
 		}
 	}
 
