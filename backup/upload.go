ae24fed8de038eb0c56053e8aa535f4945db5d37
nderitualphie <aliphonzanderitu@gmail.com>
2023-06-23 13:43:17 +0300

    bak

--- backup/upload.go
+++ backup/upload.go
@@ -1,14 +1,12 @@
 package bp
 
 import (
-	"log"
-
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/service/s3"
-
-	"os"
+	"log"
 	"path/filepath"
 
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"os"
 	//"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
@@ -53,55 +51,109 @@ func uploadFile(uploadFileDir string) error {
 	}
 	log.Print("Session created successfully")
 
-	upFile, err := os.Open(uploadFileDir)
+	dir, err := os.Open(uploadFileDir)
 	if err != nil {
-		if os.IsNotExist(err) {
-			log.Printf("Directory does not exist: %s", uploadFileDir)
-		} else {
-			log.Printf("Error opening directory: %v", err)
-		}
-
+		log.Printf("Error opening directory: %v", err)
+		return err
 	}
-	defer upFile.Close()
+	defer dir.Close()
 	log.Print("Directory opened successfully")
 
-	files, err := upFile.Readdirnames(-1)
+	files, err := dir.Readdir(-1)
 	if err != nil {
-		log.Printf("Error reading filenames: %v", err)
+		log.Printf("Error reading directory: %v", err)
 		return err
 	}
-	log.Printf("Filenames read successfully: %v", files)
-
-	errChan := make(chan error)
-	doneChan := make(chan bool)
+	log.Printf("Files found in directory: %d", len(files))
 
 	for _, file := range files {
-		go func(file string) {
-			f, err := os.Open(filepath.Join(uploadFileDir, file))
-			if err != nil {
-				errChan <- err
-				return
-			}
-			defer f.Close()
-			doneChan <- true
-		}(file)
+		if file.IsDir() {
+			// Skip directories
+			continue
+		}
+
+		filePath := filepath.Join(uploadFileDir, file.Name())
+		f, err := os.Open(filePath)
+		if err != nil {
+			log.Printf("Error opening file: %v", err)
+			return err
+		}
+		defer f.Close()
+		log.Printf("File opened: %s", filePath)
+
+		_, err = s3.New(sess).PutObject(&s3.PutObjectInput{
+			Bucket: aws.String(os.Getenv("BUCKET_NAME")),
+			Key:    aws.String(file.Name()),
+			Body:   f,
+		})
+		if err != nil {
+			log.Printf("Error uploading file: %v", err)
+			return err
+		}
+		log.Printf("File uploaded: %s", filePath)
 	}
 
-	for range files {
-		<-doneChan
-	}
-	log.Print("All files processed successfully")
-
-	bucket := os.Getenv("BUCKET_NAME")
-	_, err = s3.New(sess).PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(uploadFileDir),
-	})
-	if err != nil {
-		log.Printf("Error uploading file: %v", err)
-		return err
-	}
-	log.Print("File uploaded successfully")
-
+	log.Print("Upload completed successfully")
 	return nil
 }
+
+//func uploadFile(uploadFileDir string) error {
+//	sess, err := createSession()
+//	if err != nil {
+//		log.Printf("Error creating session: %v", err)
+//		return err
+//	}
+//	log.Print("Session created successfully")
+//
+//	upFile, err := os.Open(uploadFileDir)
+//	if err != nil {
+//		if os.IsNotExist(err) {
+//			log.Printf("Directory does not exist: %s", uploadFileDir)
+//		} else {
+//			log.Printf("Error opening directory: %v", err)
+//		}
+//
+//	}
+//	defer upFile.Close()
+//	log.Print("Directory opened successfully")
+//
+//	files, err := upFile.Readdirnames(-1)
+//	if err != nil {
+//		log.Printf("Error reading filenames: %v", err)
+//		return err
+//	}
+//	log.Printf("Filenames read successfully: %v", files)
+//
+//	errChan := make(chan error)
+//	doneChan := make(chan bool)
+//
+//	for _, file := range files {
+//		go func(file string) {
+//			f, err := os.Open(filepath.Join(uploadFileDir, file))
+//			if err != nil {
+//				errChan <- err
+//				return
+//			}
+//			defer f.Close()
+//			doneChan <- true
+//		}(file)
+//	}
+//
+//	for range files {
+//		<-doneChan
+//	}
+//	log.Print("All files processed successfully")
+//
+//	bucket := os.Getenv("BUCKET_NAME")
+//	_, err = s3.New(sess).PutObject(&s3.PutObjectInput{
+//		Bucket: aws.String(bucket),
+//		Key:    aws.String(uploadFileDir),
+//	})
+//	if err != nil {
+//		log.Printf("Error uploading file: %v", err)
+//		return err
+//	}
+//	log.Print("File uploaded successfully")
+//
+//	return nil
+//}
